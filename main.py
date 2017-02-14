18c5c56c420490e7bbb84ddade0866c6836558e4
codingma <454226880@qq.com>
2017-02-14 18:06:32 +0800

    微调

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # coding=UTF-8
 from preprocess.data_process import preprocess
-from rule.submission_by_rule4 import get_result_by_last_three_weeks_mean
+from rule.submission_by_rule import get_result_by_last_week_with_weight
 from rule.extra_fix import extra_fix
 
 #预处理数据，获取切分后的数据
 preprocess()
-#按当前效果最好的前三星期均值作为结果
-get_result_by_last_three_weeks_mean()
+#算结果
+get_result_by_last_week_with_weight()
 #最后做少量人工调整
 extra_fix()
