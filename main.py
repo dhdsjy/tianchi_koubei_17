751842c36fd488227e664a1ed4192f8522cfa8e8
codingma <454226880@qq.com>
2017-02-14 17:18:33 +0800

    取消注释

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from rule.submission_by_rule4 import get_result_by_last_three_weeks_mean
 from rule.extra_fix import extra_fix
 
 #预处理数据，获取切分后的数据
-# preprocess()
+preprocess()
 #按当前效果最好的前三星期均值作为结果
 get_result_by_last_three_weeks_mean()
 #最后做少量人工调整
